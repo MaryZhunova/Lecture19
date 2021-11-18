bb71c1f9f389af52970760898cfb6a1036c96a1a
MaryZhunova <maryzhunova@yandex.ru>
2021-11-18 13:19:10 +0300

    key/id updated

--- app/src/main/java/com/example/recipe/data/api/Constants.kt
+++ app/src/main/java/com/example/recipe/data/api/Constants.kt
@@ -6,7 +6,7 @@ package com.example.recipe.data.api
 class Constants {
     companion object{
         val url = "https://api.edamam.com/api/recipes/v2"
-        val key = "e906d60b053299bd251af78fa297f507"
+        val key = "1c70177c0b5d56057410b74bd9ae1283"
         val id = "f2aed758"
     }
 }
